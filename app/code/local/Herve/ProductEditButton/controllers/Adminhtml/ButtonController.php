e8a45e7a40f6d0d22bdc427dfa81b70ac0c94f13
Shouryauni <shourya@uniterrene.com>
2017-11-29 18:12:33 +0530

    Added new logic for pulling the media images to the proper folder while click in the sync button

--- app/code/local/Herve/ProductEditButton/controllers/Adminhtml/ButtonController.php
+++ app/code/local/Herve/ProductEditButton/controllers/Adminhtml/ButtonController.php
@@ -14,41 +14,7 @@ class Herve_ProductEditButton_Adminhtml_ButtonController extends Mage_Adminhtml_
     {
         // Retrieve product id from which the button has been clicked
         $productId = $this->getRequest()->getParam('id');
-        $_product = Mage::getModel('catalog/product')->load($productId);
-        $opt_title = "Metal";
-        print_r($_product->getSku());
-        $product_data_custom = $this->getLayout()->getBlockSingleton('Canvassignages_Liveeditor_Block_Monblock')->customCanvasPrint($_product->getSku(),$opt_title);
-     //  echo "<pre>"; print_r($product_data_custom); echo "</pre>";
-
-       foreach ($product_data_custom as $key=> $custom_value) {
-
-        $data = substr($_POST['imageData'], strpos($_POST['imageData'], ",") + 1);
-        $decodedData = base64_decode($data);
-        $fp = fopen("img_06264.png", 'wb');
-        fwrite($fp, $decodedData);
-        fclose($fp);
-        //echo $custom_value['option_label'];
-
-       // $img=$uploader->getUploadedFileName($custom_value['img_url']);
-       // print_r($img);      
-
-        //$io = new Varien_Io_File();
-
-       // echo "<pre>"; print_r($io) ;
-       //$io->mkdir(Mage::getBaseUrl('media').'/import/images', 0775);
-        // $imageName = $custom_value["img_url"];
-
-        // get the image from the import dir
-       // $import = Mage::getBaseDir('media') . DS . 'import/' . $imageName;
-        //$io->checkAndCreateFolder(Mage::getBaseUrl('media').'xyzzzzzz');
-            // if ( !file_exists(Mage::getBaseUrl('media').'custom_option_image/'.$custom_value['option_label']) && !empty($custom_value['option_label'])) {
-            //  mkdir(Mage::getBaseUrl('media').'xyzzzzzz', 0777, true);
-            //  echo $custom_value['option_label'];
-            // }
-       }
-      
-
-        die;
+       
 
         /**
          *
@@ -71,11 +37,15 @@ class Herve_ProductEditButton_Adminhtml_ButtonController extends Mage_Adminhtml_
             $media_image_label  = $image->getLabel();
 
 
-            //print_r($media_image_path);
+           
             
             $mage_dir_url =  Mage::getBaseDir();
 
             $j=1;
+           
+
+           
+             
 
             if (!file_exists($mage_dir_url.'/jewelzhq'.'/metal'.'/'.$productId.'/'.$media_image_label)) {
                 mkdir($mage_dir_url.'/jewelzhq'.'/metal'.'/'.$productId.'/'.$media_image_label, 0777, true);
@@ -89,9 +59,11 @@ class Herve_ProductEditButton_Adminhtml_ButtonController extends Mage_Adminhtml_
             //copy($media_image_path , $mage_dir_url.'/jewelzhq'.'/metal'.'/'.$productId.'/'.$media_image_label.'/1_1.png');
 
 
+            
+
         }
 
-        exit();
+         exit();
 
 
 
