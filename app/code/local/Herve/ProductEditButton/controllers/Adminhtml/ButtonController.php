f821321307c949fbd88b5d2043d0ff0209263837
uniterrenerina <rina@uniterrene.com>
2017-11-29 16:53:55 +0530

    test

--- app/code/local/Herve/ProductEditButton/controllers/Adminhtml/ButtonController.php
+++ app/code/local/Herve/ProductEditButton/controllers/Adminhtml/ButtonController.php
@@ -14,6 +14,41 @@ class Herve_ProductEditButton_Adminhtml_ButtonController extends Mage_Adminhtml_
     {
         // Retrieve product id from which the button has been clicked
         $productId = $this->getRequest()->getParam('id');
+        $_product = Mage::getModel('catalog/product')->load($productId);
+        $opt_title = "Metal";
+        print_r($_product->getSku());
+        $product_data_custom = $this->getLayout()->getBlockSingleton('Canvassignages_Liveeditor_Block_Monblock')->customCanvasPrint($_product->getSku(),$opt_title);
+     //  echo "<pre>"; print_r($product_data_custom); echo "</pre>";
+
+       foreach ($product_data_custom as $key=> $custom_value) {
+
+        $data = substr($_POST['imageData'], strpos($_POST['imageData'], ",") + 1);
+        $decodedData = base64_decode($data);
+        $fp = fopen("img_06264.png", 'wb');
+        fwrite($fp, $decodedData);
+        fclose($fp);
+        //echo $custom_value['option_label'];
+
+       // $img=$uploader->getUploadedFileName($custom_value['img_url']);
+       // print_r($img);      
+
+        //$io = new Varien_Io_File();
+
+       // echo "<pre>"; print_r($io) ;
+       //$io->mkdir(Mage::getBaseUrl('media').'/import/images', 0775);
+        // $imageName = $custom_value["img_url"];
+
+        // get the image from the import dir
+       // $import = Mage::getBaseDir('media') . DS . 'import/' . $imageName;
+        //$io->checkAndCreateFolder(Mage::getBaseUrl('media').'xyzzzzzz');
+            // if ( !file_exists(Mage::getBaseUrl('media').'custom_option_image/'.$custom_value['option_label']) && !empty($custom_value['option_label'])) {
+            //  mkdir(Mage::getBaseUrl('media').'xyzzzzzz', 0777, true);
+            //  echo $custom_value['option_label'];
+            // }
+       }
+      
+
+        die;
 
         /**
          *
